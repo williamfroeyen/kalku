d5adc1f35ed37bc8b90240cf6dcce7cac8591097
williamfroeyen <williamfroeyen@gmail.com>
2025-08-20 19:56:08 +0200

    Ny hundeårkalkulator

---
+++ diverse/hundear.js
@@ -0,0 +1,10 @@
+const input1 = document.querySelector("#input1");
+input1.addEventListener("input", (e) => {
+    const inputValue = e.target.value;
+    let outputValue = "";
+    if (inputValue.length > 0 && !isNaN(inputValue)) {
+        outputValue = 16 * Math.log(inputValue) + 31;
+        outputValue = Math.round((outputValue + Number.EPSILON));
+    }
+    document.querySelector("#output1").value = outputValue;
+});
