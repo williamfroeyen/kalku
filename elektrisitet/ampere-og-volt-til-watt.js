6927748ac41257ce6dcb0868b4ecf7f3ea1d6454
williamfroeyen <williamfroeyen@gmail.com>
2025-08-30 20:17:55 +0200

    Nye kalkulatorer

---
+++ elektrisitet/ampere-og-volt-til-watt.js
@@ -0,0 +1,26 @@
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const outputTextElement = document.querySelector("#oneResultText")
+
+input1.addEventListener("input", updateResult);
+input2.addEventListener("input", updateResult);
+
+function updateResult() {
+    let input1Value = parseFloat(input1.value);
+    let input2Value = parseFloat(input2.value);
+    let outputValue = "";
+
+    if (!isNaN(input1Value) && !isNaN(input2Value)) {
+        outputValue = input1Value * input2Value;
+        outputValue = Math.round((outputValue + Number.EPSILON)*100)/100;
+        outputValueLength = (outputValue + '').replace('.', '').length;
+
+        if (outputValueLength > 12) {
+            outputTextElement.textContent="For mange tall";
+        } else {
+            outputTextElement.textContent=`Resultat: ${outputValue}`;
+        }
+    } else {
+        outputTextElement.textContent="Resultat: ";
+    }
+};
