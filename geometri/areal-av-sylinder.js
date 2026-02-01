0a0a58c29f4b7bef98529ae4401e3589a8880b3a
William Frøyen <williamfroyen@outlook.com>
2026-02-01 19:17:56 +0100

    Nye geometrikalkulatorer

---
+++ geometri/areal-av-sylinder.js
@@ -0,0 +1,26 @@
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const outputTextElement = document.querySelector("#oneResultText")
+
+input1.addEventListener("input", updateResult);
+input2.addEventListener("input", updateResult);
+
+function updateResult() {
+    let input1Value = parseFloat(input1.value);
+    let input2Value = parseFloat(input2.value);
+    let outputValue = "";
+
+    if (!isNaN(input1Value) && !isNaN(input2Value)) {
+        outputValue = 2*Math.PI*input1Value**2 + 2*Math.PI*input1Value*input2Value;
+        outputValue = Math.round((outputValue + Number.EPSILON)*10000)/10000;
+        outputValueLength = (outputValue + '').replace('.', '').length;
+
+        if (outputValueLength > 12) {
+            outputTextElement.textContent="For mange tall";
+        } else {
+            outputTextElement.textContent=`Areal: ${outputValue}`;
+        }
+    } else {
+        outputTextElement.textContent="Areal: ";
+    }
+};
