1471939f8371c97732d1a6e3c4f769f01c1fa347
William Froeyen <williamfroyen@outlook.com>
2026-02-12 15:45:51 +0100

    Forbedret mediankalkulator og fikset feil

--- statistikk/mediankalkulator.js
+++ statistikk/mediankalkulator.js
@@ -1,42 +1,49 @@
-const input = document.querySelector("#textareastat");
+const userInput = document.querySelector("#textareastat");
 const outputTextElement = document.querySelector("#oneResultText");
 const errorMessageContainer = document.querySelector("#errorMessageContainer");
 const errorMessageText = document.querySelector("#errorMessageText");
 
-input.addEventListener("input", charCheck);
+userInput.addEventListener("input", charCheck);
 
 function charCheck() {
-    let inputValueString = String(input.value);
-    const allowedChars = /^[0-9.,\s]+$/;
+    let inputValueString = String(userInput.value);
+    inputValueString = inputValueString.replace(/\s+/g, "");
+    const allowedChars = /^[0-9.,]+$/;
 
     const isCharsAllowed = allowedChars.test(inputValueString);
     
     if (isCharsAllowed === false && !!inputValueString) {
         errorMessageContainer.classList.remove("hidden");
-        errorMessageText.textContent="Bare tall og komma er tillatt";
+        errorMessageText.textContent="Bare tall, komma og punktum er tillatt";
     } else if (isCharsAllowed === true && !!inputValueString) {
         errorMessageContainer.classList.add("hidden");
         calculateResult(inputValueString);
     } else {
         errorMessageContainer.classList.add("hidden");
         outputTextElement.textContent=`Median:`;
-    }
+    };
 };
 
 function calculateResult(inputValueString) {
     const inputValueArray = inputValueString.split(",");
-    const arrayTrimmed = inputValueArray.map(item => item.replaceAll(/\s/g, ''));
-    const arrayRemovedEmptyStrings = arrayTrimmed.filter(Boolean);
+    const arrayRemovedEmptyStrings = inputValueArray.filter(Boolean);
     const arrayNumbers = arrayRemovedEmptyStrings.map(Number);
     const arraySorted = arrayNumbers.sort((a, b) => a - b);
     let medianValue = 0;
 
+    if (arrayNumbers.length === 0) {
+        errorMessageContainer.classList.remove("hidden");
+        errorMessageText.textContent = "Fyll feltet med tall";
+        outputTextElement.textContent = "Median:";
+        return;
+    };
+    
     if (arrayNumbers.some(n => Number.isNaN(n))) {
         errorMessageContainer.classList.remove("hidden");
         errorMessageText.textContent = "Ugyldig tallformat (sjekk bruk av punktum/komma)";
         outputTextElement.textContent = "Median:";
         return;
-    }
+    };
 
     if (arraySorted.length % 2 === 0) {
         const middleArrayLongLength = Math.floor(arraySorted.length / 2);
@@ -48,13 +55,13 @@ function calculateResult(inputValueString) {
     } else {
         const middleOfArray = Math.floor(arraySorted.length / 2);
         medianValue = Number(arraySorted[middleOfArray]);
-    }
+    };
 
     if (medianValue > 100000000000) {
         medianValue = medianValue.toExponential(4);
     } else {
         medianValue = Math.round((medianValue + Number.EPSILON)*10000)/10000;
-    }
+    };
     outputTextElement.textContent = `Median: ${medianValue}`;
 };
 
