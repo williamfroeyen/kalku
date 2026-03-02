f1e4274776f4d2072201cf4e7b302ee14f552813
William Froeyen <williamfroyen@outlook.com>
2026-03-02 19:41:36 +0100

    Nye tidskalkulatorer

---
+++ tid/sekunder-til-timer.js
@@ -0,0 +1,21 @@
+const inputElement = document.querySelector("#input1");
+const outputElement = document.querySelector("#output1");
+const resultExpTextElement = document.querySelector("#resultsExpText");
+
+inputElement.addEventListener("input", (e) => {
+    const inputString = e.target.value.trim();
+    const validFormat = /^-?[0-9]*([.,][0-9]*)?$/.test(inputString);
+    let outputString = "";
+
+    if (validFormat && inputString.length > 0 && inputString !== "-") {
+        const inputNum = parseFloat(inputString.replace(",", "."));
+
+        if (!isNaN(inputNum)) {
+            let calculated = inputNum / 3600;
+            calculated = Math.round((calculated + Number.EPSILON)*10000)/10000;
+            outputString = String(calculated).replace(".", ",");
+        };
+    };
+    
+    outputElement.value = outputString;
+});
