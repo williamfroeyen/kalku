025e534454c3feffc7a127da858ce696dcf38622
William Froeyen <williamfroyen@outlook.com>
2026-03-04 20:38:33 +0100

    Ny magnetisk fluks kalkulator

---
+++ elektromagnetisme/magnetisk-fluks-kalkulator.js
@@ -0,0 +1,34 @@
+let outputValue = "";
+
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const input3 = document.querySelector("#input3");
+
+const outputTextElement = document.querySelector("#oneResultText");
+const resultExpTextElement = document.querySelector("#resultsExpText");
+
+input1.addEventListener("input", updateResult);
+input2.addEventListener("input", updateResult);
+input3.addEventListener("input", updateResult);
+
+function updateResult() {
+    let input1Value = parseFloat(input1.value.replace(",", "."));
+    let input2Value  = parseFloat(input2.value.replace(",", "."));
+    let input3Value  = parseFloat(input3.value.replace(",", "."));
+
+    if (!isNaN(input1Value) && !isNaN(input2Value) && !isNaN(input3Value)) {
+        outputValue = input1Value * input2Value * Math.cos(input3Value * Math.PI / 180);;
+        outputValue = Math.round((outputValue + Number.EPSILON)*10000)/10000;
+
+
+        outputValue = String(outputValue).replace(".", ",");
+
+        if (outputValue.length > 10) {
+            outputTextElement.textContent="For mange tall";
+        } else {
+            outputTextElement.textContent=`Fluks: ${outputValue} Wb`;
+        };
+    } else {
+        outputTextElement.textContent="Fluks: ";
+    };
+};
