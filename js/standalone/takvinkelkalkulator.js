50a003349346cd703cb32f5784654bd8b877eef2
William Froeyen <williamfroyen@outlook.com>
2026-04-14 18:57:38 +0200

    Mer modulær og robust takvinkelkalkulator-kode

--- js/standalone/takvinkelkalkulator.js
+++ js/standalone/takvinkelkalkulator.js
@@ -1,3 +1,126 @@
+import { rounding, prepInput } from '../core/calcfunctions.js';
+
+const verticalHeight = document.querySelector("#loddretttakhoyde");
+const horizontalLength = document.querySelector("#vannretttaklengde");
+const slopedLength = document.querySelector("#skrataklengde");
+
+const errorMessageContainer = document.querySelector("#errorMessageContainer");
+const errorMessageText = document.querySelector("#errorMessageText");
+
+const resultsList = document.querySelector("#resultsList");
+const outputDecimals = 3;
+
+verticalHeight.addEventListener("input", inputEvent);
+horizontalLength.addEventListener("input", inputEvent);
+slopedLength.addEventListener("input", inputEvent);
+
+function inputEvent() {
+    errorMessageContainer.classList.add("hidden");
+    resultsList.innerHTML = "";
+
+    if (verticalHeight.value === "" && horizontalLength.value !== "" && slopedLength.value !== "") {
+        const numberArray = prepInput([horizontalLength, slopedLength]);
+        if (errorCheck(numberArray)) calcVerticalHeight(numberArray);
+
+    } else if (verticalHeight.value !== "" && horizontalLength.value === "" && slopedLength.value !== "") {
+        const numberArray = prepInput([verticalHeight, slopedLength]);
+        if (errorCheck(numberArray)) calcHorizontalLength(numberArray);
+
+    } else if (verticalHeight.value !== "" && horizontalLength.value !== "" && slopedLength.value === "") {
+        const numberArray = prepInput([verticalHeight, horizontalLength]);
+        if (errorCheck(numberArray)) calcSlopedLength(numberArray);
+    } else if (verticalHeight.value !== "" && horizontalLength.value !== "" && slopedLength.value !== "") {
+        displayError("Fyll maksimalt to felt.");
+    };
+};
+
+function errorCheck(numberArray) {
+    if (numberArray === "invalidInput") {
+        displayError("Bare tall, komma og punktum er tillatt.");
+        return false;
+
+    } else if (numberArray === "tooManyPeriods") {
+        displayError("Kun ett komma eller punktum er tillatt.");
+        return false;
+    };
+
+    return true;
+};
+
+function calcVerticalHeight(numberArray) {
+    const [horizontalNum, slopedNum] = numberArray;
+
+    if (slopedNum <= horizontalNum) {
+        displayError("Skrå taklengde må være lengre enn vannrett lengde.");
+        return;
+    };
+
+    const verticalNum = Math.sqrt(slopedNum**2 - horizontalNum**2);
+    calcSlopeAngle(verticalNum, horizontalNum, slopedNum);
+
+};
+
+function calcHorizontalLength(numberArray) {
+    const [verticalNum, slopedNum] = numberArray;
+
+    if (slopedNum <= verticalNum) {
+        displayError("Skrå taklengde må være lengre enn høyden.");
+        return;
+    };
+    
+    const horizontalNum = Math.sqrt(slopedNum**2 - verticalNum**2);
+    calcSlopeAngle(verticalNum, horizontalNum, slopedNum);
+};
+
+function calcSlopedLength(numberArray) {
+    const [verticalNum, horizontalNum] = numberArray;
+    const slopedNum = Math.sqrt(verticalNum**2 + horizontalNum**2);
+
+    calcSlopeAngle(verticalNum, horizontalNum, slopedNum);
+};
+
+function calcSlopeAngle(verticalNum, horizontalNum, slopedNum) {
+    const slopeAngleRad = Math.atan(verticalNum/horizontalNum);
+    const slopeAngleDeg = (slopeAngleRad * (180 / Math.PI));
+    const mmpermeter = Math.tan(slopeAngleRad)*1000;
+    const slopePercent = (verticalNum/horizontalNum)*100;
+    const resultArray = [verticalNum, horizontalNum, slopedNum, slopeAngleDeg, mmpermeter, slopePercent];
+
+    prepareOutput(resultArray);
+};
+
+function prepareOutput(resultArray) {
+    const roundedArray = resultArray.map(result => rounding(result, outputDecimals));
+    const [verticalString, horizontalString, slopedString, slopeAngleDeg, mmpermeter, slopePercent] = roundedArray;
+
+    let outputItems = [];
+    outputItems.push(`Skrå taklengde: ${slopedString} m`);
+    outputItems.push(`Loddrett takhøyde: ${verticalString} m`);
+    outputItems.push(`Vannrett avstand til midten: ${horizontalString} m`);
+    outputItems.push(`Takvinkel (grader): ${slopeAngleDeg}°`);
+    outputItems.push(`Takhelling (mm per meter): ${mmpermeter} mm/m`);
+    outputItems.push(`Takhelling (%): ${slopePercent}%`);
+    renderOutput(outputItems);
+};
+
+function renderOutput(outputItems) {
+    resultsList.innerHTML = "";
+    outputItems.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = String(item).replace(".", ",");
+        resultsList.appendChild(li);
+    });
+};
+
+function displayError(message) {
+    errorMessageContainer.classList.remove("hidden");
+    errorMessageText.textContent = message;
+};
+
+
+/*
+import { round2Num } from '../core/calcfunctions.js';
+
 const loddretttakhoydeElement = document.querySelector("#loddretttakhoyde");
 const vannretttaklengdeElement = document.querySelector("#vannretttaklengde");
 const skrataklengdeElement = document.querySelector("#skrataklengde");
@@ -7,6 +130,9 @@ const resultsList = document.querySelector("#resultsList");
 loddretttakhoydeElement.addEventListener("input", finnUkjentLengde);
 vannretttaklengdeElement.addEventListener("input", finnUkjentLengde);
 skrataklengdeElement.addEventListener("input", finnUkjentLengde);
+
+const twodecimals = 2;
+const threedecimals = 3;
 
 function finnUkjentLengde() {
     const loddretttakhoydeInput = parseFloat(loddretttakhoydeElement.value.replace(',', '.'));
@@ -23,10 +149,10 @@ function finnUkjentLengde() {
     } else if (!isNaN(loddretttakhoydeInput) && !isNaN(vannretttaklengdeInput) && isNaN(skrataklengdeInput)) {
         // FINN LENGDEN AV SKRÅTAKET
         const skrataklengde = Math.sqrt(loddretttakhoydeInput**2 + vannretttaklengdeInput**2);
-        const skrataklengdeOutput = avrunding(skrataklengde, 1000);
-        const loddretttakhoydeOutput = avrunding(loddretttakhoydeInput, 1000);
-        const vannretttaklengdeOutput = avrunding(vannretttaklengdeInput, 1000);
-        finnHelning(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklengdeOutput);
+        const skrataklengdeOutput = round2Num(skrataklengde, threedecimals);
+        const loddretttakhoydeOutput = round2Num(loddretttakhoydeInput, threedecimals);
+        const vannretttaklengdeOutput = round2Num(vannretttaklengdeInput, threedecimals);
+        findSlope(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklengdeOutput);
 
     } else if (!isNaN(loddretttakhoydeInput) && isNaN(vannretttaklengdeInput) && !isNaN(skrataklengdeInput)) {
         // FINN VANNRETT TAKLENGDE
@@ -36,10 +162,11 @@ function finnUkjentLengde() {
         }
 
         const vannretttaklengde = Math.sqrt(skrataklengdeInput**2 - loddretttakhoydeInput**2);
-        const vannretttaklengdeOutput = avrunding(vannretttaklengde, 1000);
-        const skrataklengdeOutput = avrunding(skrataklengdeInput, 1000);
-        const loddretttakhoydeOutput = avrunding(loddretttakhoydeInput, 1000);
-        finnHelning(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklengdeOutput);
+        const vannretttaklengdeOutput = round2Num(vannretttaklengde, threedecimals);
+        const skrataklengdeOutput = round2Num(skrataklengdeInput, threedecimals);
+        const loddretttakhoydeOutput = round2Num(loddretttakhoydeInput, threedecimals);
+        console.log(vannretttaklengde);
+        findSlope(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklengdeOutput);
  
     } else if (isNaN(loddretttakhoydeInput) && !isNaN(vannretttaklengdeInput) && !isNaN(skrataklengdeInput)) {
         // FINN LODDRETT TAKHØYDE
@@ -49,11 +176,11 @@ function finnUkjentLengde() {
         }
 
         const loddretttakhoyde = Math.sqrt(skrataklengdeInput**2 - vannretttaklengdeInput**2);
-        const loddretttakhoydeOutput = avrunding(loddretttakhoyde, 1000);
-        const vannretttaklengdeOutput = avrunding(vannretttaklengdeInput, 1000);
-        const skrataklengdeOutput = avrunding(skrataklengdeInput, 1000);
+        const loddretttakhoydeOutput = round2Num(loddretttakhoyde, threedecimals);
+        const vannretttaklengdeOutput = round2Num(vannretttaklengdeInput, threedecimals);
+        const skrataklengdeOutput = round2Num(skrataklengdeInput, threedecimals);
         
-        finnHelning(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklengdeOutput);
+        findSlope(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklengdeOutput);
  
     } else if (isNaN(loddretttakhoydeInput) && isNaN(vannretttaklengdeInput) && isNaN(skrataklengdeInput)) {
         resultsList.innerHTML = "";
@@ -64,15 +191,11 @@ function finnUkjentLengde() {
     };
 };
 
-function avrunding(variabel, noyaktighet) {
-    return Math.round((variabel + Number.EPSILON)*noyaktighet)/noyaktighet;
-};
-
-function finnHelning(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklengdeOutput) {
+function findSlope(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklengdeOutput) {
     const takvinkel = Math.atan(loddretttakhoydeOutput/vannretttaklengdeOutput);
-    const takvinkelOutput = avrunding((takvinkel * (180 / Math.PI)), 100);
-    const mmpermeterOutput = avrunding((Math.tan(takvinkel)*1000), 100);
-    const helningsprosentOutput = avrunding((loddretttakhoydeOutput/vannretttaklengdeOutput)*100, 100);
+    const takvinkelOutput = round2Num((takvinkel * (180 / Math.PI)), twodecimals);
+    const mmpermeterOutput = round2Num((Math.tan(takvinkel)*1000), twodecimals);
+    const helningsprosentOutput = round2Num((loddretttakhoydeOutput/vannretttaklengdeOutput)*100, twodecimals);
 
     let outputItems = [];
     outputItems.push(`Skrå taklengde: ${skrataklengdeOutput} m`);
@@ -84,11 +207,4 @@ function finnHelning(loddretttakhoydeOutput, vannretttaklengdeOutput, skrataklen
     renderList(outputItems);
 };
 
-function renderList(outputItems) {
-    resultsList.innerHTML = "";
-    outputItems.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = String(item).replace(".", ",");
-        resultsList.appendChild(li);
-    });
-};
+*/
